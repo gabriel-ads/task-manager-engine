4d0ad4b8b42327fd54a8e60ccb3421df76510c32
Gabriel Alves <gabriel.alves@hotmail.com>
2024-03-30 14:53:31 -0300

    feature: Added new integration tests and removed unused imports

--- routes/Tasks.js
+++ routes/Tasks.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const jwt = require("jsonwebtoken");
-const { privateKey } = require("../globalVariables.js");
 const authenticate = require("../middleware/authenticate.js");
 const Task = require("../core/task.js");
 
